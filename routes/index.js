7db01a3bacd87a1d18e70b438457b810fcd59e52
ChemaRuperez <jm.rupoza@gmail.com>
2015-08-06 22:16:07 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ var sessionController = require('../controllers/session_controller');
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -34,7 +35,8 @@ router.get('/quizes?search=:texto_busqueda', quizController.answer);
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
-
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+                            sessionController.loginRequired, commentController.publish);
 
 router.get('/author/',                     quizController.author);
 
